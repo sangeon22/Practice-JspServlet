cbca0f26f5906c052303d7d9d0124a9c10469b52
sangeon22 <tkddjsdl33@hanmail.net>
2022-10-20 23:33:23 +0900

    UTF-8, Response Header (setContentType)

--- JSPPrj/src/main/java/com/eonion/web/Test.java
+++ JSPPrj/src/main/java/com/eonion/web/Test.java
@@ -4,15 +4,24 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("hello")
 public class Test extends HttpServlet{
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		PrintWriter out = resp.getWriter();
-		out.println("init");
+		// 사용자에게 보내는 방식이 UTF-8
+		// 브라우저에서 읽을 수 있도록 Response Header에 심는다.
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("안녕 hi !!");
+		
+		
 	}
 }
