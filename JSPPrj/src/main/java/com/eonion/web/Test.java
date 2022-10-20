47be49c0776927ad71ef56e3c2d76b29b6b2bb2d
sangeon22 <tkddjsdl33@hanmail.net>
2022-10-20 23:18:14 +0900

    XML & Annotation URL Mapping

--- JSPPrj/src/main/java/com/eonion/web/Test.java
+++ JSPPrj/src/main/java/com/eonion/web/Test.java
@@ -2,12 +2,15 @@ package com.eonion.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello")
 public class Test extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
