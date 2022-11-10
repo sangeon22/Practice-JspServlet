f1d4e5c88a57a5110caad0992379da63fe0abad6
sangeon22 <tkddjsdl33@hanmail.net>
2022-11-10 22:37:37 +0900

    Refactor: "Session을 통한 calc 기능"
    Application -> Session 객체로 사용
    상태를 저장하기 위해 사용
    Session 범주 내에서 사용 가능(현재 접속한 사용자, 세션마다 공간이 개별적으로 부여)

--- JSPPrj/src/main/java/com/eonion/web/Calc2.java
+++ JSPPrj/src/main/java/com/eonion/web/Calc2.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
@@ -15,7 +16,12 @@ public class Calc2 extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//Application 저장소로 사용 -> Servlet Context를 통한 상태유지, 자원공유?
+		//Application 전역에서 사용 가능
 		ServletContext application = request.getServletContext();
+		
+		//Session 객체로 사용 -> 상태를 저장하기 위해 사용
+		//Session 범주 내에서 사용 가능(현재 접속한 사용자, 세션마다 공간이 개별적으로 부여)
+		HttpSession session = request.getSession();
 		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; Charset=UTF-8");
@@ -27,10 +33,13 @@ public class Calc2 extends HttpServlet {
 		if(!v_.equals("")) v = Integer.parseInt(v_);
 		
 		if(op.equals("=")) {
+//			int x = (Integer) application.getAttribute("value");
+			int x = (Integer) session.getAttribute("value");
 			
-			int x = (Integer) application.getAttribute("value");
 			int y = v;
-			String operator = (String) application.getAttribute("op");
+			
+//			String operator = (String) application.getAttribute("op");
+			String operator = (String) session.getAttribute("op");
 			
 			int result = 0;
 			
@@ -41,8 +50,11 @@ public class Calc2 extends HttpServlet {
 				
 		}else {
 			//컬렉션프레임워크 Map이라고 생각 (key, value)
-			application.setAttribute("value", v);
-			application.setAttribute("op", op);
+//			application.setAttribute("value", v);
+//			application.setAttribute("op", op);
+			
+			session.setAttribute("value", v);
+			session.setAttribute("op", op);
 		}
 		
 	
