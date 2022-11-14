f770030da425c67d94e9d919b55e4d742b7a7c7a
sangeon22 <tkddjsdl33@hanmail.net>
2022-11-14 23:03:11 +0900

    Feat: "계산기 동적인 페이지 서블릿 기능2"
    JSP를 사용하지 않고 동적인 페이지(서버페이지)를 서블릿으로 구현에 필요한 코드
    Html 정적페이지를 Java 서버단의 Servlet에 추가

--- JSPPrj/src/main/java/com/eonion/web/Calc3.java
+++ JSPPrj/src/main/java/com/eonion/web/Calc3.java
@@ -2,6 +2,9 @@ package com.eonion.web;
 
 import java.io.IOException;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,86 +19,38 @@ public class Calc3 extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; Charset=UTF-8");
+		Cookie[] cookies = request.getCookies();	
 		
-		//1. Application 저장소로 사용 -> Servlet Context를 통한 상태유지, 자원공유?
-		//   Application 전역에서 사용 가능
-		ServletContext application = request.getServletContext();
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
 		
-		//2. Session 객체로 사용 -> 상태를 저장하기 위해 사용
-		//   Session 범주 내에서 사용 가능(현재 접속한 사용자, 세션마다 공간이 개별적으로 부여)
-		HttpSession session = request.getSession();
-		
-		//3. 사용자에게 생성해 보냈던 쿠키를 읽는 배열
-		Cookie[] cookies = request.getCookies();
-	
-		
-		String v_ = request.getParameter("v");
-		String op = request.getParameter("operator");
-				
-		int v = 0;
-		if(!v_.equals("")) v = Integer.parseInt(v_);
-		
-		if(op.equals("=")) {
-//			int x = (Integer) application.getAttribute("value");
-//			int x = (Integer) session.getAttribute("value");
-			
-			// Cookies에서 하나씩 쿠키를 꺼내면서 키값에 맞는 밸류를 꺼냄
-			int x = 0;
-			for (Cookie c : cookies) {
-				if(c.getName().equals("value")) {
-					x = Integer.parseInt(c.getValue());
+		String exp = "";
+		if(cookies != null)
+			for(Cookie c : cookies)
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
 					break;
 				}
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			
-			String operator = "";
-			for (Cookie c : cookies) {
-				if(c.getName().equals("op")) {
-					operator = c.getValue();
-					break;
-				}
-			}
-			
-				
-			int y = v;
-//			String operator = (String) application.getAttribute("op");
-//			String operator = (String) session.getAttribute("op");
-			
-			int result = 0;
-			
-			if(operator.equals("+")) result = x + y;
-			else result = x - y;
-			
-			response.getWriter().printf("결과값 = %d", result);
-				
-		}else {
-			//컬렉션프레임워크 Map이라고 생각 (key, value)
-//			application.setAttribute("value", v);
-//			application.setAttribute("op", op);
-			
-//			session.setAttribute("value", v);
-//			session.setAttribute("op", op);
-			
-			// 쿠키를 만들어서 response Header에 심어서 클라이언트에게 전달
-			Cookie valueCookie = new Cookie("value", String.valueOf(v));
-			Cookie opCookie = new Cookie("op", op);
-			
-			// 쿠키 옵션 - URL에 맞는 경우에만 쿠키를 가져오도록
-			valueCookie.setPath("/calc2");
-			opCookie.setPath("/calc2");
-			
-			// 쿠키의 만료시간을 주는 옵션(브라우저를 닫아도 유효)
-			valueCookie.setMaxAge(24*60*60);
-			
-			response.addCookie(valueCookie);
-			response.addCookie(opCookie);
-			
-			response.sendRedirect("calc2.html");
 		}
-		
-	
+		else {
+			exp += (value == null)?"":value;
+			exp += (operator == null)?"":operator;
+			exp += (dot == null)?"":dot;
+		}
+		Cookie expCookie = new Cookie("exp", exp);	
+		response.addCookie(expCookie);
+		response.sendRedirect("calcpage");
 	}
+		
 
 }
