35a0e54df8064caf86e4a4b7912ebaea20c127ae
sangeon22 <tkddjsdl33@hanmail.net>
2022-12-01 15:13:53 +0900

    Refactor: 공지사항 게시글 상세페이지 MVC model2로 리팩토링
    1. 기존 MVC model1에서 View(JSP)와 Controller(Servlet)를 분리
    2. request 저장소를 Model(bean)으로 활용하여 JSP에 전달
    3. list.jsp에서 JSP가 아닌 Controller를 통해 접근하도록 UIRL 매핑 변경

---
+++ JSPPrj/src/main/java/com/eonion/web/controller/NoticeDetailController.java
@@ -0,0 +1,65 @@
+package com.eonion.web.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/notice/detail")
+public class NoticeDetailController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String url = "jdbc:oracle:thin:@localhost:1523/xepdb1";
+		String sql = "SELECT * FROM NOTICE WHERE ID=?";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "", "");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+
+			rs.next();
+
+			String title = rs.getString("TITLE");
+			String writerId = rs.getString("WRITER_ID");
+			Date regdate = rs.getDate("REGDATE");
+			String hit = rs.getString("HIT");
+			String files = rs.getString("FILES");
+			String content = rs.getString("CONTENT");
+			
+			// request 객체 속성에 저장
+			request.setAttribute("title", title);
+			request.setAttribute("writerId", writerId);
+			request.setAttribute("regdate", regdate);
+			request.setAttribute("hit", hit);
+			request.setAttribute("files", files);
+			request.setAttribute("content", content);
+			
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// detail.jsp 뷰를 작성하기 위해 현재 상태값을 request 객체로 forward를 통해 넘겨줌
+		request.getRequestDispatcher("/notice/detail.jsp").forward(request, response);
+	}
+
+}
