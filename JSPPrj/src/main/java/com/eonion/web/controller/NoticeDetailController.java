9bec6cdbc15122a0c83b9e79227565a8f28f3149
sangeon22 <tkddjsdl33@hanmail.net>
2023-01-04 22:03:27 +0900

    Feat: 게시글 상세페이지 Controller, Service 분리
    NoticeService에 구현해놓은 메서드(id를 파라미터로 받음)를 통해 컨트롤러 로직 분리

--- JSPPrj/src/main/java/com/eonion/web/controller/NoticeDetailController.java
+++ JSPPrj/src/main/java/com/eonion/web/controller/NoticeDetailController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.eonion.web.entity.Notice;
+import com.eonion.web.service.NoticeService;
 
 @WebServlet("/notice/detail")
 public class NoticeDetailController extends HttpServlet {
@@ -22,44 +23,10 @@ public class NoticeDetailController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
-		String url = "jdbc:oracle:thin:@localhost:1523/xepdb1";
-		String sql = "SELECT * FROM NOTICE WHERE ID=?";
-
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection(url, "", "");
-			PreparedStatement st = con.prepareStatement(sql);
-			st.setInt(1, id);
-			ResultSet rs = st.executeQuery();
-
-			rs.next();
-
-			String title = rs.getString("TITLE");
-			String writerId = rs.getString("WRITER_ID");
-			Date regdate = rs.getDate("REGDATE");
-			String hit = rs.getString("HIT");
-			String files = rs.getString("FILES");
-			String content = rs.getString("CONTENT");
-			
-			Notice notice = new Notice(id, title, writerId, regdate, hit, files, content);
-			
-			request.setAttribute("n", notice);
-			
-			 /* // request 객체 속성에 저장 request.setAttribute("title", title);
-			 * request.setAttribute("writerId", writerId); request.setAttribute("regdate",
-			 * regdate); request.setAttribute("hit", hit); request.setAttribute("files",
-			 * files); request.setAttribute("content", content);
-			 */			
-			rs.close();
-			st.close();
-			con.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		NoticeService noticeService = new NoticeService();
+		Notice notice = noticeService.getNotice(id);
+		request.setAttribute("n", notice);
 		
 		// detail.jsp 뷰를 작성하기 위해 현재 상태값을 request 객체로 forward를 통해 넘겨줌
 		request.getRequestDispatcher("/WEB-INF/view/notice/detail.jsp").forward(request, response);
