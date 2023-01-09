6a8e43cdc9841dd827f0abee63fc52745ddf82ea
sangeon22 <tkddjsdl33@hanmail.net>
2023-01-09 22:42:32 +0900

    Feat: 공지사항 게시물 관리자 페이지 기능 추가(1)
    1. MVC 구조를 위한 Controller, View(JSP) 추가
    2. Service 로직 메서드 정의(명칭 및 리턴값만 정의)

---
+++ JSPPrj/src/main/java/com/eonion/web/controller/admin/notice/ListController.java
@@ -0,0 +1,64 @@
+package com.eonion.web.controller.admin.notice;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.eonion.web.entity.Notice;
+import com.eonion.web.entity.NoticeView;
+import com.eonion.web.service.NoticeService;
+
+@WebServlet("/admin/notice/list")
+public class ListController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String field_ = request.getParameter("field");
+		String query_ = request.getParameter("keyWord");
+		String page_ = request.getParameter("page");
+		
+		String field = "title";
+		if(field_ != null && !field_.equals("")) {
+			if(field_.equals("writerId")) {
+				field_ = "writer_id";
+			}
+			field = field_;				
+		}
+		
+		String query = "";
+		if (query_ != null && !query_.equals("")) {
+			query = query_;
+		}
+		
+		int page = 1;
+		if (page_ != null && !page_.equals("")) {
+			page = Integer.parseInt(page_);
+		}
+		
+		NoticeService noticeService = new NoticeService();
+		List<NoticeView> list = noticeService.getNoticeList(field, query, page);
+		int count = noticeService.getNoticeCount(field, query);
+		
+		request.setAttribute("list", list);
+		request.setAttribute("count", count);
+		
+		//forward
+		request.getRequestDispatcher("/WEB-INF/view/admin/board/notice/list.jsp").forward(request, response);
+		
+	}
+
+}
--- JSPPrj/src/main/java/com/eonion/web/service/NoticeService.java
+++ JSPPrj/src/main/java/com/eonion/web/service/NoticeService.java
@@ -15,6 +15,34 @@ import com.eonion.web.entity.Notice;
 import com.eonion.web.entity.NoticeView;
 
 public class NoticeService {
+	
+	public int removeNoticeAll(int[] ids){
+		return 0;
+	}
+	
+	public int pubNoticeAll(int[] ids) {
+		
+		return 0;
+	}
+	
+	public int insertNotice(Notice notice){
+		return 0;
+	}
+	
+	public int deleteNotice(int id){
+		
+		return 0;
+	}
+	
+	public int updateNotice(Notice notice){
+		
+		return 0;
+	}
+	
+	List<Notice> getNoticeNewestList(){
+		return null;
+	}
+	
 	public List<NoticeView> getNoticeList() {
 		return getNoticeList("title", "", 1);
 	}
