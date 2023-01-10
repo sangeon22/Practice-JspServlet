6cb449be3a64d728b55920642c3a302b4d32ffb9
sangeon22 <tkddjsdl33@hanmail.net>
2023-01-10 22:59:58 +0900

    Feat: 공지사항 게시물 관리자 페이지 기능 추가(3) - 공지사항 게시글 등록 기능
    1. JSP(View)
    2. Controller - Get요청(작성폼이동), Post요청(게시글 작성) 정의

---
+++ JSPPrj/src/main/java/com/eonion/web/controller/admin/notice/RegController.java
@@ -0,0 +1,33 @@
+package com.eonion.web.controller.admin.notice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/board/notice/reg")
+public class RegController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 공지사항 작성폼으로 이동
+		request.getRequestDispatcher("/WEB-INF/view/admin/board/notice/reg.jsp").forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		String isOpen = request.getParameter("open");
+		
+		PrintWriter out = response.getWriter();
+		out.printf("title : %s<br>", title);
+		out.printf("content : %s<br>", content);
+		out.printf("isOpen : %s<br>", isOpen);
+		
+		
+	}
+}
