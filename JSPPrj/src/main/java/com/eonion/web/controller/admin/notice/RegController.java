ee375e345ba20f23f22b49eb2966331a4c541da2
sangeon22 <tkddjsdl33@hanmail.net>
2023-01-14 22:53:49 +0900

    Feat: 파일 업로드 기능 구현(1)
    1. JSP view 단 POST 방식 인코딩 타입 지정
    2. 컨트롤러 단 @MultipartConfig 어노테이션 설정

--- JSPPrj/src/main/java/com/eonion/web/controller/admin/notice/RegController.java
+++ JSPPrj/src/main/java/com/eonion/web/controller/admin/notice/RegController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import com.eonion.web.entity.Notice;
 import com.eonion.web.service.NoticeService;
 
+@MultipartConfig(
+		fileSizeThreshold = 1024*1024,
+		maxFileSize = 1024*1024*50,
+		maxRequestSize = 1024*1024*50*5
+)
 @WebServlet("/admin/board/notice/reg")
 public class RegController extends HttpServlet{
 	@Override
@@ -25,6 +31,8 @@ public class RegController extends HttpServlet{
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
 		String isOpen = request.getParameter("open");
+		
+		System.out.println(title);
 		boolean pub = false;
 		if(isOpen != null) {
 			pub = true;
